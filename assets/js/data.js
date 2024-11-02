5cdfaaad81a7756a84ea5d62313a459ab5277891
freetimezp <kraftukrnet@ukr.net>
2024-11-03 00:39:39 +0200

    03. js-portfolio-148-project-page. GSAP animation.

---
+++ assets/js/data.js
@@ -0,0 +1,152 @@
+export const galleryItems = [
+    {
+        title: "Beyond The Summit",
+        copy: `
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+            Adipisci quis repellat inventore quas. Modi obcaecati aspernatur 
+            voluptatibus quam voluptate officia.
+        `,
+        director: "Alex Honnold",
+        cinematographer: "Jimmy Chin"
+    },
+    {
+        title: "Olympiam's Journey",
+        copy: `
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+            Adipisci quis repellat inventore quas. Modi obcaecati aspernatur 
+            voluptatibus quam voluptate officia.
+        `,
+        director: "Sarah Chen",
+        cinematographer: "Marcus Doherty"
+    },
+    {
+        title: "Velocity",
+        copy: `
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+            Adipisci quis repellat inventore quas. Modi obcaecati aspernatur 
+            voluptatibus quam voluptate officia.
+        `,
+        director: "Yuki Tanaka",
+        cinematographer: "Amanda Ronney"
+    },
+    {
+        title: "The Last Whistle",
+        copy: `
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+            Adipisci quis repellat inventore quas. Modi obcaecati aspernatur 
+            voluptatibus quam voluptate officia.
+        `,
+        director: "Tome Brady",
+        cinematographer: "Roger Deakins"
+    },
+    {
+        title: "Court of Dream",
+        copy: `
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+            Adipisci quis repellat inventore quas. Modi obcaecati aspernatur 
+            voluptatibus quam voluptate officia.
+        `,
+        director: "Malik Jhonson",
+        cinematographer: "Elena Petrova"
+    },
+    {
+        title: "Waves of Change",
+        copy: `
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+            Adipisci quis repellat inventore quas. Modi obcaecati aspernatur 
+            voluptatibus quam voluptate officia.
+        `,
+        director: "Wes Anderson",
+        cinematographer: "Jimmy Chin"
+    },
+    {
+        title: "Chess Prodigy",
+        copy: `
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+            Adipisci quis repellat inventore quas. Modi obcaecati aspernatur 
+            voluptatibus quam voluptate officia.
+        `,
+        director: "Ava Duballi",
+        cinematographer: "Rachel Morrison"
+    },
+    {
+        title: "Marathon of Mind",
+        copy: `
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+            Adipisci quis repellat inventore quas. Modi obcaecati aspernatur 
+            voluptatibus quam voluptate officia.
+        `,
+        director: "Christopher Nolan",
+        cinematographer: "Hoyte van Hoitem"
+    },
+    {
+        title: "Urban Evolution",
+        copy: `
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+            Adipisci quis repellat inventore quas. Modi obcaecati aspernatur 
+            voluptatibus quam voluptate officia.
+        `,
+        director: "Lucy Evolution",
+        cinematographer: "Thiery Arbo"
+    },
+    {
+        title: "The Referee's Call",
+        copy: `
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+            Adipisci quis repellat inventore quas. Modi obcaecati aspernatur 
+            voluptatibus quam voluptate officia.
+        `,
+        director: "Martin Scorsese",
+        cinematographer: "Rodrigo Prestoyan"
+    },
+    {
+        title: "Esports Revolution",
+        copy: `
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+            Adipisci quis repellat inventore quas. Modi obcaecati aspernatur 
+            voluptatibus quam voluptate officia.
+        `,
+        director: "Edgar Wright",
+        cinematographer: "Bill Poup"
+    },
+    {
+        title: "The Ironman Story",
+        copy: `
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+            Adipisci quis repellat inventore quas. Modi obcaecati aspernatur 
+            voluptatibus quam voluptate officia.
+        `,
+        director: "Kathryn Bigyle",
+        cinematographer: "Barry Arnold"
+    },
+    {
+        title: "Dance of Matador",
+        copy: `
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+            Adipisci quis repellat inventore quas. Modi obcaecati aspernatur 
+            voluptatibus quam voluptate officia.
+        `,
+        director: "Pedro Almodovar",
+        cinematographer: "Jimmy Chin"
+    },
+    {
+        title: "Way of the Warrior",
+        copy: `
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+            Adipisci quis repellat inventore quas. Modi obcaecati aspernatur 
+            voluptatibus quam voluptate officia.
+        `,
+        director: "Hirokazu Kore-eda",
+        cinematographer: "Rachel Kondo"
+    },
+    {
+        title: "Breaking the Ice",
+        copy: `
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+            Adipisci quis repellat inventore quas. Modi obcaecati aspernatur 
+            voluptatibus quam voluptate officia.
+        `,
+        director: "Ryan Cooglar",
+        cinematographer: "Ryuto Morrison"
+    },
+];
